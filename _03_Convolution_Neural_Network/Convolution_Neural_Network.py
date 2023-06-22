977f153833453dc18c7afe4e5d6bf5285e0d8819
Alizen-1009 <82800539+Alizen-1009@users.noreply.github.com>
2023-06-22 19:28:04 +0800

    333

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -15,29 +15,21 @@ from torch.utils.data import DataLoader
 
 # 判断是否有GPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-img_height = 32
-img_width = 32
     
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1, bias=False)
 
-def conv3x3(in_channels, out_channels, kernel_size = 3,stride=1, padding=1):
-    return nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
-                     stride=stride, padding=padding, bias=False)
 
-
-# Resnet_50  中的残差块
+# Resnet 的残差块
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
-        self.mid_channels = out_channels//4
-        self.conv1 = conv3x3(in_channels, self.mid_channels, kernel_size=1, stride=stride, padding=0)#Resnet50 中，从第二个残差块开始每个layer的第一个残差块都需要一次downsample
-        self.bn1 = nn.BatchNorm2d(self.mid_channels)
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(self.mid_channels, self.mid_channels)
-        self.bn2 = nn.BatchNorm2d(self.mid_channels)
-        self.conv3 = conv3x3(self.mid_channels, out_channels,kernel_size=1,padding=0)
-        self.bn3 = nn.BatchNorm2d(out_channels)
-        self.downsample_0 = conv3x3(in_channels,out_channels,kernel_size=1,stride=1,padding=0)
+        self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
 
     def forward(self, x):
@@ -47,42 +39,36 @@ class ResidualBlock(nn.Module):
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-        out = self.conv3(out)
-        out = self.bn3(out)
         if self.downsample:
             residual = self.downsample(x)
-        else:
-            residual = self.downsample_0(x)
-
         out += residual
-        out = self.bn3(out)
         out = self.relu(out)
         return out
 
 
 # ResNet定义
-class NeuralNetwork(nn.Module):
+class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=10):
-        super(NeuralNetwork, self).__init__()
-        self.conv = conv3x3(3, 64,kernel_size=7,stride=2)
-        self.bn = nn.BatchNorm2d(64)
-        self.max_pool = nn.MaxPool2d(3,2,padding=1)
-        self.layer1 = self.make_layer(block, 64, 256, layers[0])
-        self.layer2 = self.make_layer(block, 256, 512, layers[1], 2)
-        self.layer3 = self.make_layer(block, 512, 1024, layers[2], 2)
-        self.layer4 = self.make_layer(block, 1024, 2048, layers[3], 2)
-        self.avg_pool = nn.AvgPool2d(3,stride=1,padding=1)
-        self.fc = nn.Linear(math.ceil(img_height/32)*math.ceil(img_width/32)*2048, num_classes)
+        super(ResNet, self).__init__()
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        self.layer1 = self.make_layer(block, 16, layers[0])
+        self.layer2 = self.make_layer(block, 32, layers[1], 2)
+        self.layer3 = self.make_layer(block, 64, layers[2], 2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
 
-    def make_layer(self, block, in_channels, out_channels, blocks, stride=1):
+    def make_layer(self, block, out_channels, blocks, stride=1):
         downsample = None
-        if (stride != 1):
+        if (stride != 1) or (self.in_channels != out_channels):
             downsample = nn.Sequential(
-                conv3x3(in_channels, out_channels, kernel_size=3,stride=stride),
+                conv3x3(self.in_channels, out_channels, stride=stride),
                 nn.BatchNorm2d(out_channels))
         layers = []
-        layers.append(block(in_channels, out_channels, stride, downsample))
-
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
         for i in range(1, blocks):
             layers.append(block(out_channels, out_channels))
         return nn.Sequential(*layers)
@@ -90,16 +76,15 @@ class NeuralNetwork(nn.Module):
     def forward(self, x):
         out = self.conv(x)
         out = self.bn(out)
-        out = self.max_pool(out)
+        out = self.relu(out)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = self.layer4(out)
         out = self.avg_pool(out)
-        out = out.view( -1,math.ceil(img_height/32)*math.ceil(img_width/32)*2048)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
         return out
-
-
+    
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
     # 保持本地训练的数据读取和这里一致
@@ -110,9 +95,9 @@ def read_data():
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 def main():
-    model = NeuralNetwork(ResidualBlock, [3, 4, 6, 3]).to(device) # 若有参数则传入参数
+    model = ResNet(ResidualBlock, [2, 2, 2]).to(device) # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
-    model.load_state_dict(torch.loads(parent_dir + '/pth/model.pth'))
+    model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
     return model
     
